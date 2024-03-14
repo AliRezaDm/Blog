421dcd9a0da7f918c30d2fdd2ee893f3b10b06d9
AliReza Doostmohammadi <alireza.dm80@gmail.com>
2024-03-14 16:21:06 +0330

    article view modified

--- article/urls.py
+++ article/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import ArticleList, ArticleDetail, ArticlePreview, search_view
+from .views import ArticleList, ArticleDetail, ArticlePreview, search_view, CategoryList
 
 
 app_name = 'article'
@@ -8,14 +8,7 @@ urlpatterns = [
 
     path ('', ArticleList.as_view(), name="home"),   
     path ('detail/<slug>', ArticleDetail.as_view(), name='detail'),
+    path ('category/<slug>', CategoryList.as_view(), name='category_list'),
     path ('search/', search_view, name='search'),
 ]
-
-
-
-
-
-
-
-
 
